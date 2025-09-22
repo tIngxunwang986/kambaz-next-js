ed572a7b74c67d90fe820b2669d0e1fa88fdaa8f
tingxun wang <wang.tingxu@northeastern.edu>
2025-09-22 18:38:56 -0400

    A1

--- app/(Kambaz)/Account/Signup/page.tsx
+++ app/(Kambaz)/Account/Signup/page.tsx
@@ -7,7 +7,7 @@ export default function Signup() {
             <input placeholder="password" type="password" className="wd-password" /><br/>
             <input placeholder="verify password"
                    type="password" className="wd-password-verify" /><br/>
-            <Link  href="Profile" > Sign up </Link><br />
-            <Link  href="Signin" > Sign in </Link>
+            <Link  href="/Account/Profile" > Sign up </Link><br />
+            <Link  href="/Account/Signin" > Sign in </Link>
         </div>
     );}
