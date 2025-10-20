358282622101d2bc7930f76faef7bf1e80da0350
tingxun wang <wang.tingxu@northeastern.edu>
2025-10-20 14:58:17 -0400

    edit

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -22,6 +22,6 @@ export default function TOC() {
                 <NavLink href="/" as={Link}>
                     Kambaz </NavLink> </NavItem>
             <NavItem>
-                <NavLink href="https://github.com/jannunzi">My GitHub</NavLink></NavItem>
+                <NavLink href="https://github.com/tIngxunwang986/kambaz-next-js/tree/A3">My GitHub</NavLink></NavItem>
         </Nav>
     );}
