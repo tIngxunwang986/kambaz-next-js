66b12f84d4cdcdc9bc532b5b5274b5292f5f72b8
tingxun wang <wang.tingxu@northeastern.edu>
2025-11-09 12:39:18 -0500

    fixed error

---
+++ app/Labs/Lab4/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ClickEvent from "./ClickEvent";
+
+export default function Lab4() {
+    return (
+        <div id="wd-lab4">
+            <h3>Lab 4 - Managing State and User Input with Forms</h3>
+
+            <ClickEvent />
+
+        </div>
+    );
+}
